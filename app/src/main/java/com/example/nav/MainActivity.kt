e295b013c9281131d01f5a81d82a4dbf15848070
CHOHEEJEONG <chohee0505@gmail.com>
2021-09-08 15:35:58 +0900

    2021-09-08

--- app/src/main/java/com/example/nav/MainActivity.kt
+++ app/src/main/java/com/example/nav/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.nav
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,30 +14,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.nav.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_community, R.id.navigation_mypage, R.id.navigation_settings
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
         mwebView.settings.javaScriptEnabled = true // 자바 스크립트 허용
         /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
@@ -41,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         mwebView.webChromeClient = WebChromeClient()
         /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
         mwebView.loadUrl("http://naver.com") //링크 주소를 Load (http://34.64.143.233:8080/)
+
+        btn_main.setOnClickListener {
+            val intent = Intent(this, MainActivity2:: class.java)
+            startActivity(intent);
+        }
     }
 
     override fun onBackPressed() {
@@ -50,4 +47,7 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+
+
 }
--- app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -17,8 +18,6 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -28,12 +27,9 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        val webView: WebView = binding.mwebView
 
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
         return root
     }
 
