f48db0c51706bd52462c9d46dea5914e54031cac
CHOHEEJEONG <chohee0505@gmail.com>
2021-09-08 19:13:36 +0900

    2021-09-08-v2

--- app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.Button
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -12,6 +13,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.nav.R
 import com.example.nav.databinding.FragmentHomeBinding
+import android.widget.RadioButton
+
+
 
 class HomeFragment : Fragment() {
 
@@ -27,8 +31,20 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val selectedOSeason: TextView = binding.selectedOSeason
+        val selectedDSeason: TextView = binding.selectedDSeason
+        val originalGroup: RadioGroup = binding.originalSeason
+        val desiredGroup: RadioGroup = binding.desiredSeason
+        val btn_transfer: Button = binding.btnTransfer
 
-
+        originalGroup.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.o_spring -> selectedOSeason.setText("spring")
+                R.id.o_summer -> selectedOSeason.setText("summer")
+                R.id.o_autumn -> selectedOSeason.setText("autumn")
+                R.id.o_winter -> selectedOSeason.setText("winter")
+            }
+        })
 
         return root
     }
