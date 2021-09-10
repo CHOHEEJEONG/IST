e8586c5f347365cb7b699337424c45b00c301c96
CHOHEEJEONG <chohee0505@gmail.com>
2021-09-10 16:31:02 +0900

    2021-09-10-v1

--- app/src/main/java/com/example/nav/MainActivity.kt
+++ app/src/main/java/com/example/nav/MainActivity.kt
@@ -7,17 +7,19 @@ import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.nav.databinding.ActivityMainBinding
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         mwebView.webViewClient = WebViewClient()
         mwebView.webChromeClient = WebChromeClient()
         /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
-        mwebView.loadUrl("http://naver.com") //링크 주소를 Load (http://34.64.143.233:8080/)
+        mwebView.loadUrl("http://34.64.143.233:8080/") //링크 주소를 Load (http://34.64.143.233:8080/)
 
         btn_main.setOnClickListener {
             val intent = Intent(this, MainActivity2:: class.java)
--- app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
@@ -1,20 +1,27 @@
 package com.example.nav.ui.home
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.nav.R
 import com.example.nav.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import android.widget.Toast
+import androidx.core.view.get
 import android.widget.RadioButton
-
+import com.example.nav.MainActivity
+import com.example.nav.MainActivity2
+import android.widget.RadioGroup
+import com.google.android.material.radiobutton.MaterialRadioButton
 
 
 class HomeFragment : Fragment() {
@@ -24,33 +31,75 @@ class HomeFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    var originalGroup: RadioGroup? = null
+    var originalButton: RadioButton? = null
+    var desiredGroup: RadioGroup? = null
+    var desiredButton: RadioButton? = null
+    var selectedOSeason: TextView? = null
+    var selectedDSeason: TextView? = null
+
+    lateinit var mainActivity: MainActivity2
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity2
+    }
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val root : View = binding.root
 
-        val selectedOSeason: TextView = binding.selectedOSeason
-        val selectedDSeason: TextView = binding.selectedDSeason
-        val originalGroup: RadioGroup = binding.originalSeason
-        val desiredGroup: RadioGroup = binding.desiredSeason
+
+        originalGroup = binding.originalSeason
+        desiredGroup = binding.desiredSeason
+        selectedOSeason = binding.selectedOSeason
+        selectedDSeason = binding.selectedDSeason
         val btn_transfer: Button = binding.btnTransfer
 
-        originalGroup.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                R.id.o_spring -> selectedOSeason.setText("spring")
-                R.id.o_summer -> selectedOSeason.setText("summer")
-                R.id.o_autumn -> selectedOSeason.setText("autumn")
-                R.id.o_winter -> selectedOSeason.setText("winter")
+        originalGroup?.setOnCheckedChangeListener { group, i ->
+            val idx = group.indexOfChild(root.findViewById(group.checkedRadioButtonId))
+            originalButton = group.getChildAt(idx) as MaterialRadioButton
+            if(originalButton != null)
+                selectedOSeason!!.text = originalButton!!.text.toString()
+        }
+
+        desiredGroup?.setOnCheckedChangeListener { group, i ->
+            val idx = group.indexOfChild(root.findViewById(group.checkedRadioButtonId))
+            desiredButton = group.getChildAt(idx) as MaterialRadioButton
+            if(desiredButton != null)
+                selectedDSeason!!.text = desiredButton!!.text.toString()
+        }
+
+
+
+        btn_transfer.setOnClickListener(View.OnClickListener {
+            fun onClick(v: View?) {
+                val originalId: Int = originalGroup!!.checkedRadioButtonId
+                originalButton = originalGroup!!.getChildAt(originalId) as RadioButton
+                Toast.makeText(mainActivity, "Selected Season : " +originalButton!!.text, Toast.LENGTH_LONG).show()
+
             }
         })
 
+        Log.e("setOnClickListener", "ENTER")
+
         return root
+
+
     }
+
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
