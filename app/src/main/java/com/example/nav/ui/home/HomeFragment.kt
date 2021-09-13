d300c5db7728f69b9aa7cda565ec2176ac9ab86a
CHOHEEJEONG <chohee0505@gmail.com>
2021-09-13 11:41:07 +0900

    2021-09-13-v1

--- app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nav/ui/home/HomeFragment.kt
@@ -121,33 +121,13 @@ class HomeFragment : Fragment() {
             getPhotoFromMyGallary()
         }
 
+        (activity as MainActivity2).setPermission()
+
         return root
-        setPermission()
 
     }
 
-    // 테드 퍼미션 설정 (카메라 사용시 권한 설정 팝업을 쉽게 구현하기 위해 사용)
-    fun setPermission() {
-        val permission = object : PermissionListener {
-            override fun onPermissionGranted() {//설정해 놓은 위험권한(카메라 접근 등)이 허용된 경우 이곳을 실행
-                Toast.makeText(this@HomeFragment.mainActivity, "요청하신 권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
-            }
 
-            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {//설정해 놓은 위험권한이 거부된 경우 이곳을 실행
-                Toast.makeText(this@HomeFragment.mainActivity, "요청하신 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        TedPermission.with(mainActivity)
-            .setPermissionListener(permission)
-            .setRationaleMessage("카메라 앱을 사용하시려면 권한을 허용해주세요.")
-            .setDeniedMessage("권한을 거부하셨습니다.앱을 사용하시려면 [앱 설정]-[권한] 항목에서 권한을 허용해주세요.")
-            .setPermissions(
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            )
-            .check()
-    }
 
 
 
